445928c6b96ad054155298ff3c2ab7e16cf6dce3
VadimTsybikov <vadtsyb000@gmail.com>
2020-06-07 22:07:58 +0300

    second task made better

--- homework2/loops/src/Loops2.java
+++ homework2/loops/src/Loops2.java
@@ -2,15 +2,30 @@ import java.util.Scanner;
 
 public class Loops2 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Введи число: ");
-        int n = scanner.nextInt();
-
-        System.out.printf("%d! = %d", n, getFactorial(n));
+        if (args.length == 1) {
+            String arg = args[0];
+            if (tryParseInt(arg)) {
+                int n = Integer.parseInt(arg);
+                System.out.printf("%d! = %d", n, getFactorial(n));
+            }
+            else
+                System.out.println("Для работы этой программы нужен целочисленный параметр командной строки!");
+        }
+        else
+            System.out.println("Для работы этой программы нужен параметр командной строки!");
     }
 
-    public static int getFactorial(int n) {
+    private static boolean tryParseInt(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        }
+        catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    private static int getFactorial(int n) {
         int factorial = 1;
         for (int i = 1; i <= n; i++) {
             factorial *= i;
