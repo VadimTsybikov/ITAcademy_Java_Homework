dd00b1a5c461d372cd8d5aa5b506293609c7a341
VadimTsybikov <vadtsyb000@gmail.com>
2020-06-09 18:00:44 +0300

    desided to make all 2.1, 2.2, 3.1, 3.2 al class Arrays1 for FOR

--- homework2/arrays/src/Arrays1.java
+++ homework2/arrays/src/Arrays1.java
@@ -6,16 +6,17 @@ public class Arrays1 {
 
     public static void main(String arg) {
         scanner = new Scanner(System.in);
+        ArrayInput();
+        ArrayOutput();
+        ArrayOutputOddPosOnly();
+    }
 
+    private static void ArrayInput() {
         System.out.print("Введи размерность массива: ");
         int arrayLen = scanner.nextInt();
 
         array = new int[arrayLen];
-        ArrayInput();
-        ArrayOutput();
-    }
 
-    private static void ArrayInput() {
         System.out.println("Заполни массив:");
         for (int i = 0; i < array.length; i++) {
             System.out.printf("array[%d]:  ", i);
@@ -26,6 +27,19 @@ public class Arrays1 {
     private static void ArrayOutput() {
         System.out.println("Вот массив:");
         for (int i = 0; i < array.length; i++)
+            System.out.print(array[i] + " ");
+    }
+
+    private static void ArrayOutputOddPosOnly() {
+        System.out.println("Вот элементы массива на четных позициях:");
+        for (int i = 0; i < array.length; i += 2)
+            System.out.print(array[i] + " ");
+    }
+
+    private static void ArrayBubbleSort()
+    {
+        System.out.println("Массив, сортированный методом пузырька:");
+        for (int i = 0; i < array.length; i += 2)
             System.out.print(array[i] + " ");
     }
 }
