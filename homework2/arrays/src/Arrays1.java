9b2c86e0ffaed44f429e5b1f432b0d5e1a55d327
VadimTsybikov <vadtsyb000@gmail.com>
2020-06-09 18:13:31 +0300

    bubble sorting implementation. failed :(

--- homework2/arrays/src/Arrays1.java
+++ homework2/arrays/src/Arrays1.java
@@ -4,11 +4,12 @@ public class Arrays1 {
     private static Scanner scanner;
     private static int[] array;
 
-    public static void main(String arg) {
+    public static void main(String[] arg) {
         scanner = new Scanner(System.in);
         ArrayInput();
         ArrayOutput();
         ArrayOutputOddPosOnly();
+        ArrayBubbleSort();
     }
 
     private static void ArrayInput() {
@@ -25,21 +26,36 @@ public class Arrays1 {
     }
 
     private static void ArrayOutput() {
-        System.out.println("Вот массив:");
+        System.out.println("\nВот массив:");
         for (int i = 0; i < array.length; i++)
             System.out.print(array[i] + " ");
     }
 
     private static void ArrayOutputOddPosOnly() {
-        System.out.println("Вот элементы массива на четных позициях:");
+        System.out.println("\nВот элементы массива на четных позициях:");
         for (int i = 0; i < array.length; i += 2)
             System.out.print(array[i] + " ");
     }
 
     private static void ArrayBubbleSort()
     {
-        System.out.println("Массив, сортированный методом пузырька:");
-        for (int i = 0; i < array.length; i += 2)
-            System.out.print(array[i] + " ");
+        //копирование оригинального массива во временный буфер, чтобы не рушить оригинальный порядок элементов
+        int[] a = new int[array.length];
+        for (int i = 0; i < a.length; i++)
+            a[i] = array[i];
+
+        //сортировка методом пузырька
+        for (int i = 0; i < a.length - 1; i++)
+            for (int j = i; j < a.length; j++) {
+                if (a[j - 1] < a[j]) {
+                    int tmp = a[j - 1];
+                    a[j - 1] = a[i];
+                    a[i] = tmp;
+                }
+            }
+
+        System.out.println("\nМассив, сортированный методом пузырька:");
+        for (int i = 0; i < a.length; i++)
+            System.out.print(a[i] + " ");
     }
 }
