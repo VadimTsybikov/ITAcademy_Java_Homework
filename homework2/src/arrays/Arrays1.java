24ca69b0ba102cf621832cfe691b2e1fbd5a82f9
VadimTsybikov <vadtsyb000@gmail.com>
2020-06-11 00:57:34 +0300

    its something wrong with shaker sort and inclusion sort, but the rest is fine

--- homework2/src/arrays/Arrays1.java
+++ homework2/src/arrays/Arrays1.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Arrays1 {
     private static Scanner scanner;         //сканер пользовательского ввода
-    private static Random random;           //генератор случайных чисел (будет использоваться для выбора типа цикла вывода на консоль)
+    private static Random random;           //генератор случайных чисел (будет использоваться для выбора типа цикла вывода на консоль, а также для перемешивания массива)
     private static int[] array;             //целочисленный массив
 
     public static void main(String[] arg) {
@@ -15,17 +15,31 @@ public class Arrays1 {
         arrayInput();
 
         System.out.println("\nВот массив:");
-        arrayOutput();
+        arrayOutput(false);
 
         System.out.println("\nВот элементы массива на четных позициях:");
-        arrayOutputOddPosOnly();
+        arrayOutput(true);
 
-        System.out.println("\nМассив, сортированный методом пузырька:");
+        System.out.println("\nМассив после сортировки выбором:");
+        arraySelectionSort();
+
+        arrayMix();
+
+        System.out.println("\nМассив после пузырьковой сортировки:");
         arrayBubbleSort();
+
+        arrayMix();
+
+        System.out.println("\nМассив после шейкерной сортировки:");
+        arrayCocktailSort();
+
+        arrayMix();
+
+        System.out.println("\nМассив после сортировки с использованием вставок:");
+        arrayInclusionSort();
     }
 
-    //вывод массива на консоль
-    //тип цикла для вывода определяется генератором случайных чисел (o_O)
+    //ввод с консоли
     private static void arrayInput() {
         System.out.print("Введи размерность массива: ");
         int arrayLen = scanner.nextInt();
@@ -39,16 +53,106 @@ public class Arrays1 {
         }
     }
 
-    private static void arrayOutput() {
-        for (int i = 0; i < array.length; i++)
-            System.out.print(array[i] + " ");
+    //вывод массива на консоль (если параметр oddOnly = true - вывести только элементы на четных позициях)
+    //тип цикла для вывода определяется генератором случайных чисел (o_O)
+    private static void arrayOutput(boolean oddOnly) {
+        //вычисление случайного числа 0..3
+        int randomValue = Math.abs(random.nextInt()) % 4;
+
+        //в зависимости от этого числа вывести массив различными циклами
+        switch (randomValue) {
+            //реализация вывода циклом FOR
+            case 0: {
+                for (int i = 0; i < array.length; i++) {
+                    //если требуется выводить только элементы на четных позициях и текущая позиция четная - пропустить итерацию
+                    if (oddOnly && (i % 2 != 0))
+                        continue;
+
+                    System.out.print(array[i] + " ");
+                }
+
+                System.out.println("\n* Выведено с помощью for");
+                break;
+            }
+
+            //реализацич вывода циклом WHILE
+            case 1: {
+                int i = 0;
+                while (i < array.length) {
+                    if (!(oddOnly && (i % 2 != 0))) {
+                        System.out.print(array[i] + " ");
+                    }
+                    i++;
+                }
+
+                System.out.println("\n* Выведено с помощью while");
+                break;
+            }
+
+            //реализация вывода циклом DO-WHILE
+            case 2: {
+                int i = 0;
+                do {
+                    if (!(oddOnly && (i % 2 != 0))) {
+                        System.out.print(array[i] + " ");
+                    }
+                    i++;
+                } while (i < array.length);
+
+                System.out.println("\n* Выведено с помощью do-while");
+                break;
+            }
+
+            //реализация вывода циклом FOREACH
+            case 3: {
+                int i = 0;
+                for (int item : array) {
+                    if (!(oddOnly && (i % 2 != 0))) {
+                        System.out.print(item + " ");
+                    }
+                    i++;
+                }
+
+                System.out.println("\n* Выведено с помощью foreach");
+                break;
+            }
+        }
     }
 
-    private static void arrayOutputOddPosOnly() {
-        for (int i = 0; i < array.length; i += 2)
-            System.out.print(array[i] + " ");
+    //перемешивание элементов массива (для последующей демонстрации сортировки)
+    private static void arrayMix() {
+        for (int i = 0; i < array.length; i++) {
+            int destIndex = Math.abs(random.nextInt() + i) % array.length;
+            int tmp = array[i];
+            array[i] = array[destIndex];
+            array[destIndex] = tmp;
+        }
+
+        System.out.println("\nМассив перемешан:");
+        arrayOutput(false);
     }
 
+    //сортировка выбором
+    private static void arraySelectionSort() {
+        for (int i = array.length - 1; i > 0; i--) {
+            int max = array[0];
+            int maxPos = 0;
+
+            for (int j = 1; j <= i; j++) {
+                if (max < array[j]) {
+                    max = array[j];
+                    maxPos = j;
+                }
+            }
+
+            int tmp = array[i];
+            array[i] = array[maxPos];
+            array[maxPos] = tmp;
+        }
+        arrayOutput(false);
+    }
+
+    //сортировка пузырьком
     private static void arrayBubbleSort()
     {
         for (int i = 0; i < array.length - 1; i++) {
@@ -60,6 +164,50 @@ public class Arrays1 {
                 }
             }
         }
-        arrayOutput();
+        arrayOutput(false);
+    }
+
+    //шейкерная сортировка
+    private static void arrayCocktailSort()
+    {
+        int lbound = 0;
+        int ubound = array.length - 1;
+
+        while (lbound < ubound) {
+            for (int i = lbound; i < ubound; i++) {
+                if (array[i + 1] < array[i]) {
+                    int tmp = array[i + 1];
+                    array[i + 1] = array[i];
+                    array[i] = tmp;
+                }
+            }
+            ubound--;
+
+            for (int i = ubound; i < lbound; i--) {
+                if (array[i - 1] > array[i]) {
+                    int tmp = array[i - 1];
+                    array[i - 1] = array[i];
+                    array[i] = tmp;
+                }
+            }
+            lbound++;
+        }
+        arrayOutput(false);
+    }
+
+    //сортировка с использованием вставок
+    private static void arrayInclusionSort() {
+        for (int i = 1; i < array.length; i++) {
+            int val = array[i];
+            int valPos = i;
+
+            while ((valPos > 0) && (array[valPos] > array[valPos - 1])) {
+                array[valPos] = array[valPos - 1];
+                valPos--;
+            }
+
+            array[valPos] = val;
+        }
+        arrayOutput(false);
     }
 }
